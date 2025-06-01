4e96383d39ca4434e23af3a65893dbfef6b9ce61
maskface02 <qqx@gmail.com>
2025-06-01 19:00:43 +0100

    header

--- excution.c
+++ excution.c
@@ -1,10 +1,21 @@
-#include "minishell.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   excution.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zatais <zatais@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/01 19:00:17 by zatais            #+#    #+#             */
+/*   Updated: 2025/06/01 19:00:32 by zatais           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "minishell.h"
 
 //	bdit lik chwya hhhh
 void	command_execution(t_read_line *read)
 {
-	// pwd 
+	// pwd
 	if (ft_strncmp(read->arg_split[0], "pwd",
 			ft_strlen(read->arg_split[0])) == 0 && read->arg_split[1] == NULL)
 	{
